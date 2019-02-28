766430953cb2b01eeecb1c1f6b444b9ab9837529
Olivier1234 <olivier-2006@hotmail.fr>
2019-02-28 21:41:22 +0100

    add promise and countdown

--- build/assets/js/playpage/play.js
+++ build/assets/js/playpage/play.js
@@ -6,6 +6,7 @@ import Player, * as player from './player.js';
 
 const table = document.createElement('table');
 const root = document.getElementById('root');
+const para = document.createElement("p");
 
 var players = [];
 
@@ -35,8 +36,9 @@ var extractInfosFromData = (data) => {
     mainContainer[0].removeChild(document.getElementById('start_button'));
 
     generatePlayers();
+    conteur();
     //console.log(players);
-    
+
 }
 
 const generatePlayers = () => {
@@ -50,18 +52,57 @@ const generatePlayers = () => {
                 const td = document.createElement('td');
                 if (key === "photo"){
                     let img = document.createElement('img');
-                    img.setAttribute('src', element[key])
-                    img.setAttribute('class', 'player-img')
+                    img.setAttribute('src', element[key]);
+                    img.setAttribute('class', 'player-img');
                     td.appendChild(img);
                 }else{
                     text = document.createTextNode(element[key]);
                     td.appendChild(text);
                 }
                 tr.appendChild(td);
-                
+
             }
         root.appendChild(table);
     }
+}
+
+// Set the date we're counting down to
+var conteur = () => {
+    para.setAttribute('id','demo');
+    root.appendChild(para);
+    var countDownDate = new Date();
+    countDownDate.setSeconds(countDownDate.getSeconds() + 30)
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+        // Get todays date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        var milliseconde = Math.floor((distance % (1000 )));
+
+
+        // Output the result in an element with id="demo"
+        document.getElementById("demo").innerHTML = seconds + "s " + milliseconde  ;
+
+    }, 1);
+    runPromiseCountDown();
+}
+var runPromiseCountDown = () => {
+    var promise1 = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve('foo');
+        }, 30000);
+    });
+
+    promise1.then(function(value) {
+        console.log(value);
+        // expected output: "foo"
+    });
 }
 
 export {
